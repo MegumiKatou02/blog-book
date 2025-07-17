7a51cd9f81b89280f2ea4365773e76bf516c59e7
ledinhchinh0212 <khongbachofficial@gmail.com>
2025-07-17 11:36:39 +0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/blog-book/',
+  base: process.env.NODE_ENV === 'production' ? '/blog-book/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
