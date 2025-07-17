34ddde99ee17d8d6e227d6e4d2c133a7d003f812
ledinhchinh0212 <khongbachofficial@gmail.com>
2025-07-17 10:25:17 +0700

    fix: update @types/node version in package.json and package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  define: {
+    global: 'globalThis',
+  },
   base: process.env.NODE_ENV === 'production' ? '/blog-book/' : '/',
   build: {
     outDir: 'dist',
